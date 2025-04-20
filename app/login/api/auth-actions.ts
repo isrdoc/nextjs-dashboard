a776858d9bbe7f8e9219d3b6533f40d27d10de2d
IgorSrdoc <igor@srdoc.si>
2025-04-20 18:18:55 +0200

    feat: nextjs auth login

---
+++ app/login/api/auth-actions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.message.includes("CredentialsSignin")) {
+        return "Invalid credentials.";
+      }
+      return "Something went wrong.";
+    }
+    throw error;
+  }
+}
